09abccfe59e54932616aede054caad6b5dca28c2
ralismark <timmy.yao@outlook.com.au>
2017-04-18 17:08:48 +1000

    Remove converter_to for velm::vectors

--- include/velm/convert.hpp
+++ include/velm/convert.hpp
@@ -23,17 +23,6 @@ namespace velm {
 		T operator()(Args&&...) const = delete;
 	};
 
-	template <typename T, unsigned int N>
-	struct converter_to<vector<T, N>>
-	{
-		template <typename... Args,
-			std::enable_if_t<(sizeof...(Args) == N), int> = 0>
-		vector<T, N> operator()(Args&&... args) const
-		{
-			return vector<T, N>{args...};
-		}
-	};
-
 	template <typename T, typename Tup>
 	using is_vector_convertible_to = typename utility::tuple_subtype_apply<Tup,
 			utility::prepend<utility::is_callable_r, T, converter_to<T>>::template apply
